475a0ec1716ab11c4c469f4c8cfc6dba17c25768
Dmitry <Dimas23mail@gmail.com>
2025-02-11 14:45:14 +1000

    Add form delete in project

--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -18,11 +18,12 @@ def index_view(request: HttpRequest) -> HttpResponse:
 
 class ToDoListIndexView(ListView):
     template_name = "todo_list/index.html"
-    queryset = ToDoItem.objects.all()[:2]
+    queryset = ToDoItem.objects.all()[:3]
 
 
 class ToDoListView(ListView):
-    model = ToDoItem
+    #  model = ToDoItem - видит все записи
+    queryset = ToDoItem.objects.filter(archived=False)
 
 
 class ToDoListDoneView(ListView):
@@ -31,7 +32,8 @@ class ToDoListDoneView(ListView):
 
 
 class ToDoDetailView(DetailView):
-    model = ToDoItem
+    #  model = ToDoItem
+    queryset = ToDoItem.objects.filter(archived=False)
 
 
 class ToDoItemCreateView(CreateView):
@@ -49,3 +51,9 @@ class ToDoItemUpdateView(UpdateView):
 class ToDoItemDeleteView(DeleteView):
     model = ToDoItem
     success_url = reverse_lazy("todo_list:list")
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.archived = True
+        self.object.save()
+        return HttpResponseRedirect(success_url)
